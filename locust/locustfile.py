bc6cb3a9c888ee3273c823ae7f8a61b95520c82f
Thomas Yu <thomas.yu@sagebase.org>
2024-09-29 14:00:09 -0700

    Add tests

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -93,3 +93,60 @@ class ManifestGeneratorUser(HttpUser):
                 print("Existing manifest (Google Sheet) generated successfully.")
             else:
                 response.failure(f"Failed to generate existing manifest. Status code: {response.status_code}")
+
+
+
+class AssetStorageUser(HttpUser):
+    wait_time = between(1, 3)  # Simulate waiting between requests (in seconds)
+
+    def on_start(self):
+        # Optionally authenticate or set up other necessary state here
+        self.token = f"Bearer {os.environ['TOKEN']}"
+        self.headers = {"Authorization": self.token}
+
+    @task
+    def retrieve_asset_view_as_json(self):
+        """
+        Retrieve asset view table as a JSON.
+        """
+        asset_view = "syn23643253"
+        params = {"asset_view": asset_view, "return_type": "json"}
+
+        with self.client.get("/storage/assets/tables", headers=self.headers, params=params, catch_response=True) as response:
+            if response.status_code == 200:
+                response.success()
+                print(f"Retrieved asset view {asset_view} successfully.")
+            else:
+                response.failure(f"Failed to retrieve asset view {asset_view}. Status code: {response.status_code}")
+
+    @task
+    def retrieve_project_datasets(self):
+        """
+        Retrieve all datasets under a given example project.
+        """
+        asset_view = "syn23643253"
+        project_id = "syn26251192"
+        params = {"asset_view": asset_view, "project_id": project_id}
+
+        with self.client.get("/storage/project/datasets", headers=self.headers, params=params, catch_response=True) as response:
+            if response.status_code == 200:
+                response.success()
+                print(f"Retrieved all datasets for project {project_id} successfully.")
+            else:
+                response.failure(f"Failed to retrieve datasets for project {project_id}. Status code: {response.status_code}")
+
+    @task
+    def retrieve_project_datasets_HTAN(self):
+        """
+        Retrieve all datasets under a given testing HTAN project.
+        """
+        asset_view = "syn20446927"  # HTAN asset view
+        project_id = "syn32596076"  # HTAN center c
+        params = {"asset_view": asset_view, "project_id": project_id}
+
+        with self.client.get("/storage/project/datasets", headers=self.headers, params=params, catch_response=True) as response:
+            if response.status_code == 200:
+                response.success()
+                print(f"Retrieved all HTAN datasets for project {project_id} successfully.")
+            else:
+                response.failure(f"Failed to retrieve HTAN datasets for project {project_id}. Status code: {response.status_code}")
